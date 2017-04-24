e16c19c14d3fb56cd9fbe0a29c4049982ada21ef
Nader Maharmeh <nm4gg@virginia.edu>
2017-04-24 14:25:57 -0400

    Added Decryption logic to FDA

--- fda.py
+++ fda.py
@@ -20,3 +20,17 @@ payload = {
 }
 
 host = "http://127.0.0.1:8000"
+
+key = "Nader_is_awesome"
+
+def decrypt_file(filename_input, symmetric_key):
+    init_vec = b"1234567890123456"
+    AES_key = AES.new(symmetric_key, AES.MODE_CFB, init_vec)
+    output_filename = filename_input[:-4]
+    with open(filename_input, 'rb') as f:
+        raw_file = f.read()
+        data_dec = AES_key.decrypt(raw_file)
+    with open(output_filename, 'wb') as o:
+        o.write(data_dec)
+        print('Decrypted File Name:' + output_filename)
+    return True
