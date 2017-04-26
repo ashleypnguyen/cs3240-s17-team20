73a60da99a6ad7529a6fb7601ead032d8a7ad102
Seb2fq <seb2fq@virginia.edu>
2017-04-26 16:04:43 -0400

    newest commit 3

--- UserGroupForms/views.py
+++ UserGroupForms/views.py
@@ -221,4 +221,4 @@ def user_search(request):
     searched = User.objects.annotate(
         search=SearchVector('first_name', 'last_name', 'username', 'email'),
     ).filter(search=tag)
-    return render_to_response('search.html', {'searched' : searched}, context)
+    return render_to_response('search.html', RequestContext(request, {'searched' : searched}))
